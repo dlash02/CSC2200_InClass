51fa4ecff43e2d207dba9c68d5f0848fc422da82
David Lash <dlash02@gmail.com>
2024-08-22 20:00:16 -0500

    Clean out F2024

--- ExpressInClass/index2Prep.js
+++
@@ -1,9 +0,0 @@
-var express = require('Express');
-var app = express();
-var router = require('./routerPrep.js');
-//both index.js and things.js should be in same directory
-app.use('/things', router);
-let port = 3000;
-app.listen( port, () => {
-    console.log(`listing on http://localhost:${port}`);
-});
